b8268ce59cbd24f29ac1986eaafc257f952c88f3
François Pluchino <francois.pluchino@gmail.com>
2018-06-24 23:19:19 +0200

    Add base of app

---
+++ src/app.js
@@ -0,0 +1,39 @@
+/*
+ * This file is part of the Fxp Satis Serverless package.
+ *
+ * (c) François Pluchino <francois.pluchino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const compression = require('compression');
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
+const app = express();
+const router = express.Router({});
+
+if ('test' === process.env.NODE_ENV) {
+    router.use(compression());
+}
+
+router.use(cors());
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(awsServerlessExpressMiddleware.eventContext());
+
+router.get('/', (req, res) => {
+    res.json({message: 'Hello world!'});
+    //res.json(req.apiGateway.event);
+});
+
+router.post('/hooks', (req, res) => {
+    res.json({message: 'Hello world!'});
+    //res.json(req.apiGateway.event);
+});
+
+app.use('/', router);
+
+module.exports = app;
