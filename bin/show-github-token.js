1388b77c7a17b665b7434548b260cc76528042a7
François Pluchino <francois.pluchino@gmail.com>
2018-07-05 17:56:06 +0200

    Add show github token command

---
+++ bin/show-github-token.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+/*
+ * This file is part of the Fxp Satis Serverless package.
+ *
+ * (c) François Pluchino <francois.pluchino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+'use strict';
+
+require('dotenv').config();
+const program = require('commander');
+const utils = require('./utils/utils');
+const createStorage = require('./utils/storage').createStorage;
+
+program
+    .description('Create or generate a Github token')
+    .option('-l, --local', 'Use the local storage', false)
+    .parse(process.argv);
+
+utils.spawn('node bin/config -e')
+    .then(async () => {
+        let storage = createStorage(program);
+
+        return await storage.get('github-token');
+    })
+    .then((res) => {
+        if (null === res) {
+            console.info(`The Github token is not generated`);
+        } else {
+            console.info(`The Github token is "${res}"`);
+        }
+    })
+    .catch(utils.displayError);
