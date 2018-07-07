e4bd2926f3b6be70283e907fb4e6ca51fd89d0af
François Pluchino <francois.pluchino@gmail.com>
2018-07-07 12:58:27 +0200

    Add config class

---
+++ src/configs/Config.js
@@ -0,0 +1,82 @@
+/*
+ * This file is part of the Fxp Satis Serverless package.
+ *
+ * (c) François Pluchino <francois.pluchino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import merge from 'lodash.merge';
+
+/**
+ * @author François Pluchino <francois.pluchino@gmail.com>
+ */
+export default class Config
+{
+    static defaultConfig = {
+        'github-domains': ['github.com'],
+        'github-oauth': {}
+    };
+
+    constructor() {
+        this.config = Config.defaultConfig;
+    }
+
+    /**
+     * Merge the new config values with the existing ones (overriding).
+     *
+     * @param {Object} config The new config
+     */
+    merge(config) {
+        this.config = merge(this.config, config);
+    }
+
+    /**
+     * Returns a setting.
+     *
+     * @param {string} key The key
+     *
+     * @return {*}
+     */
+    get(key) {
+        let keys = key.split('[');
+        let config = this.all();
+
+        for (let i = 0; i < keys.length; ++i) {
+            let subKey = keys[i].replace(/^[\['"`]+|[\]'"`]+$/g, '');
+
+            if (config.hasOwnProperty(subKey)) {
+                if (keys.length - 1 > 0 && i < keys.length - 1) {
+                    config = config[subKey];
+                } else {
+                    return config[subKey];
+                }
+            } else {
+                break;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Get all config values.
+     *
+     * @return {Object}
+     */
+    all() {
+        return this.config;
+    }
+
+    /**
+     * Check if the setting key is defined.
+     *
+     * @param {string} key The key
+     *
+     * @return {boolean}
+     */
+    has(key) {
+        return null !== this.get(key);
+    }
+}
---
+++ test/configs/ConfigTest.js
@@ -0,0 +1,56 @@
+/*
+ * This file is part of the Fxp Satis Serverless package.
+ *
+ * (c) François Pluchino <francois.pluchino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import test from 'ava';
+import Config from '../../src/configs/Config';
+
+
+test('test config to get default values', t => {
+    let config = new Config();
+
+    t.deepEqual(config.get('github-domains'), ['github.com']);
+    t.deepEqual(config.get('github-oauth'), {});
+});
+
+test('test config to get default all values', t => {
+    let config = new Config();
+
+    t.deepEqual(config.all(), Config.defaultConfig);
+});
+
+test('test config to get nonexistent value', t => {
+    let config = new Config();
+
+    t.deepEqual(config.get('invalid-key'), null);
+});
+
+test('test config to check if key exists', t => {
+    let config = new Config();
+
+    t.true(config.has('github-domains'));
+    t.true(config.has('github-oauth'));
+    t.false(config.has('invalid-key'));
+    t.false(config.has('github-oauth[invalid-key]'));
+});
+
+test('test config to merge with new config', t => {
+    let config = new Config();
+
+    t.deepEqual(config.get('github-oauth[github.com]'), null);
+    t.deepEqual(config.get('github-oauth["github.com"]'), null);
+
+    config.merge({
+        'github-oauth': {
+            'github.com': 'MY_TOKEN'
+        }
+    });
+
+    t.deepEqual(config.get('github-oauth[github.com]'), 'MY_TOKEN');
+    t.deepEqual(config.get('github-oauth["github.com"]'), 'MY_TOKEN');
+});
