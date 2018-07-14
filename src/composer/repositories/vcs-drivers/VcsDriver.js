74fa1220c8de04149d7d136a0c94bfd335fc35c2
François Pluchino <francois.pluchino@gmail.com>
2018-07-14 17:15:31 +0200

    Implement the github driver

--- src/composer/repositories/vcs-drivers/VcsDriver.js
+++ src/composer/repositories/vcs-drivers/VcsDriver.js
@@ -8,6 +8,8 @@
  */
 
 import Config from '../../../configs/Config';
+import RemoteFilesystem from '../../utils/RemoteFilesystem';
+import {dateToRfc3339} from '../../../utils/date';
 
 /**
  * A driver implementation for driver.
@@ -19,21 +21,22 @@ export default class VcsDriver
     /**
      * Constructor.
      *
-     * @param {Object} repoConfig The repository config
-     * @param {Config} config     The config
+     * @param {Object}           repoConfig         The repository config
+     * @param {Config}           config             The config
+     * @param {RemoteFilesystem} [remoteFilesystem] The remote filesystem
      */
-    constructor(repoConfig, config) {
+    constructor(repoConfig, config, remoteFilesystem = null) {
         this.url = repoConfig['url'];
         this.repoConfig = repoConfig;
         this.config = config;
-        this.infoCache = {};
+        this.rfs = remoteFilesystem || new RemoteFilesystem(config);
     }
 
     /**
      * Checks if this driver can handle a given url.
      *
      * @param {Config}  config The config
-     * @param {string}  url    URL to validate/check
+     * @param {String}  url    URL to validate/check
      * @param {Boolean} [deep] Unless true, only shallow checks (url matching typically) should be done
      *
      * @return {Boolean}
@@ -45,9 +48,157 @@ export default class VcsDriver
     /**
      * Return the URL of the repository.
      *
-     * @return {string}
+     * @return {String}
      */
     getUrl() {
-        return this.url;
+        return '';
+    }
+
+    /**
+     * Get the data of repository.
+     *
+     * @return {Promise<Object|null>}
+     */
+    async getRepoData() {
+        return null;
+    }
+
+    /**
+     * Return the root identifier (trunk, master, default/tip ...).
+     *
+     * @return {Promise<String>}
+     */
+    async getRootIdentifier() {
+        return '';
+    }
+
+    /**
+     * Get the source info.
+     *
+     * @param {String} identifier Any identifier to a specific branch/tag/commit
+     *
+     * @return {Object} With type, url and reference keys
+     */
+    getSource(identifier) {
+        return {};
+    }
+
+    /**
+     * Get the dist info.
+     *
+     * @param {String} identifier Any identifier to a specific branch/tag/commit
+     *
+     * @return {Object} With type, url reference and shasum keys
+     */
+    getDist(identifier) {
+        return {};
+    }
+
+    /**
+     * Return true if the repository has a composer file for a given identifier,
+     * false otherwise.
+     *
+     * @param {String} identifier Any identifier to a specific branch/tag/commit
+     *
+     * @return {Promise<Boolean>} Whether the repository has a composer file for a given identifier
+     */
+    async hasComposerFile(identifier) {
+        try {
+            await this.getComposerInformation(identifier);
+            return true;
+        } catch (e) {
+        }
+
+        return false;
+    }
+
+    /**
+     * Return the composer.json file information
+     *
+     * @param {String} identifier Any identifier to a specific branch/tag/commit
+     *
+     * @return {Promise<Object>} containing all infos from the composer.json file
+     */
+    async getComposerInformation(identifier) {
+        return {};
+    }
+
+    /**
+     * Return the composer.json file base information.
+     *
+     * @param {String} identifier Any identifier to a specific branch/tag/commit
+     *
+     * @return {Promise<Object|null>} containing all infos from the composer.json file
+     */
+    async getBaseComposerInformation(identifier) {
+        let composerFileContent = await this.getFileContent('composer.json', identifier);
+
+        if (!composerFileContent) {
+            return null;
+        }
+
+        let composer = JSON.parse(composerFileContent);
+
+        if (!composer['time']) {
+            let changeDate = await this.getChangeDate(identifier);
+            if (changeDate) {
+                composer['time'] = dateToRfc3339(changeDate);
+            }
+        }
+
+        return composer;
+    }
+
+    /**
+     * Return the content of file or null if the file does not exist.
+     *
+     * @param {String} file       The file
+     * @param {String} identifier The identifier
+     *
+     * @return {Promise<String|null>}
+     */
+    async getFileContent(file, identifier) {
+        return null;
+    }
+
+    /**
+     * Get the change date for identifier.
+     *
+     * @param {String} identifier The identifier
+     *
+     * @return {Promise<Date|null>}
+     */
+    async getChangeDate(identifier) {
+        return null;
+    }
+
+    /**
+     * Return list of tags in the repository.
+     *
+     * @return {Promise<Object>} Tag names as keys, identifiers as values
+     */
+    async getTags() {
+        return {};
+    }
+
+    /**
+     * Return list of branches in the repository.
+     *
+     * @return {Promise<Object>} Branch names as keys, identifiers as values
+     */
+    async getBranches() {
+        return {};
+    }
+
+    /**
+     * Get the contents.
+     *
+     * @param {String}  url              The url
+     * @param {Boolean} fetchingRepoData Check if the repo data must be fetched
+     *
+     * @return {Promise<String|Boolean>}
+     */
+    async getContents(url, fetchingRepoData = false) {
+        return false;
     }
 }
