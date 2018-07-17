7bb0b907ffac857d930cc707e6dff855e24fb661
Peep van Puijenbroek <peep@toppershops.nl>
2018-07-17 11:22:45 +0200

    Added configuration to prevent Lambda from failing

--- src/app.js
+++ src/app.js
@@ -41,8 +41,8 @@ let storage,
     queue;
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
 
 if (isProd()) {
     app.use(compression());
