d346d5bd3b1c29528679158b77dc95f66062b239
François Pluchino <francois.pluchino@gmail.com>
2020-04-21 22:27:03 +0200

    Use search list component in repositories view

--- src/app/views/repositories/Repositories.vue
+++ src/app/views/repositories/Repositories.vue
@@ -15,46 +15,17 @@ file that was distributed with this source code.
                     {{ $t('views.repositories.title') }}
                 </v-subheader>
 
-                <v-fade-transition mode="out-in">
-                    <loading v-if="firstLoading"></loading>
+                <search-list :headers="headers" :fetch-request="fetchDataRequest">
+                    <template v-slot:no-items-icon>
+                        <v-row justify="center">
+                            <lottie width="280px" :options="{animationData: iconData}"></lottie>
+                        </v-row>
+                    </template>
 
-                    <wall-message :message="$t('views.repositories.no-items')" v-else-if="!count">
-                        <template v-slot:icon>
-                            <v-row justify="center">
-                                <lottie width="280px" :options="{animationData: iconData}"></lottie>
-                            </v-row>
-                        </template>
-
+                    <template v-slot:no-items>
                         <v-btn color="accent" ripple class="mt-3" :to="{name: 'repositories-add'}">{{ $t('views.repositories.add-first') }}</v-btn>
-                    </wall-message>
-
-                    <v-card flat v-else>
-                        <v-data-table
-                                :headers="headers"
-                                :items="items"
-                                :loading="loading"
-                                hide-default-footer
-                                item-key="id">
-                            <template slot="items" slot-scope="props">
-                                <td>
-                                    <span class="font-weight-bold">{{ props.item.packageName ? props.item.packageName : props.item.url }}</span>
-                                    <br>
-                                    <span class="font-italic">{{ props.item.type }}</span>
-                                </td>
-                                <td>
-                                    <a :href="props.item.url" target="_blank">{{ $t('source') }}</a>
-                                </td>
-                            </template>
-                            <template slot="footer" v-if="lastId !== null">
-                                <td colspan="100%" class="pl-0 pr-0 text-xs-center">
-                                    <v-btn color="accent" depressed ripple @click="fetchData()">
-                                        {{ $t('pagination.load-more') }}
-                                    </v-btn>
-                                </td>
-                            </template>
-                        </v-data-table>
-                    </v-card>
-                </v-fade-transition>
+                    </template>
+                </search-list>
             </v-col>
         </v-row>
     </v-container>
@@ -62,36 +33,28 @@ file that was distributed with this source code.
 
 <script lang="ts">
     import {MetaInfo} from 'vue-meta';
-    import {Component, Watch} from 'vue-property-decorator';
-    import {mixins} from 'vue-class-component';
-    import {AjaxListContent} from '@app/mixins/AjaxListContent';
-    import {ListResponse} from '@app/api/models/responses/ListResponse';
-    import {CodeRepository} from '@app/api/models/responses/CodeRepository';
-    import {Repositories as ApiRepositories} from '@app/api/services/Repositories';
-    import WallMessage from '@app/components/WallMessage.vue';
-    import Loading from '@app/components/Loading.vue';
+    import {Component, Vue} from 'vue-property-decorator';
+    import SearchList from '@app/components/SearchList.vue';
     import Lottie from '@app/components/Lottie.vue';
     import iconData from '@app/assets/animations/repositoryIcon.json';
+    import {ListResponse} from '@app/api/models/responses/ListResponse';
+    import {CodeRepository} from '@app/api/models/responses/CodeRepository';
+    import {FetchRequestDataEvent} from '@app/events/requests/FetchRequestDataEvent';
+    import {Repositories as ApiRepositories} from '@app/api/services/Repositories';
 
     /**
      * @author François Pluchino <francois.pluchino@gmail.com>
      */
     @Component({
-        components: {Lottie, Loading, WallMessage},
+        components: {SearchList, Lottie},
     })
-    export default class Repositories extends mixins(AjaxListContent) {
+    export default class Repositories extends Vue {
         public get iconData(): object {
             return  iconData;
         }
 
-        public metaInfo(): MetaInfo {
-            return {
-                title: this.$t('views.repositories.title') as string,
-            };
-        }
-
-        public async created(): Promise<void> {
-            this.headers = [
+        public get headers(): object[] {
+            return [
                 {   text: this.$i18n.t('views.repositories.package-name'),
                     align: 'left',
                     sortable: false,
@@ -103,31 +66,17 @@ file that was distributed with this source code.
                     value: 'url',
                 },
             ];
-
-            this.$root.$on('toolbar-search-out', async (searchValue: string) => {
-                this.search = searchValue;
-            });
-
-            await this.fetchData();
         }
 
-        public destroyed() {
-            this.$root.$off('toolbar-search-out');
+        public metaInfo(): MetaInfo {
+            return {
+                title: this.$t('views.repositories.title') as string,
+            };
         }
 
-        @Watch('search')
-        public async searchRequest(searchValue?: string): Promise<void> {
-            this.$root.$emit('toolbar-search-in', searchValue);
-            await this.fetchData(searchValue);
-        }
-
-        public async fetchDataRequest(searchValue?: string): Promise<ListResponse<CodeRepository>> {
-            const res = await this.$api.get<ApiRepositories>(ApiRepositories)
-                .list({lastId: this.lastId, search: this.search ? this.search : null}, this.previousRequest);
-
-            this.loading = false;
-
-            return res;
+        public async fetchDataRequest(event: FetchRequestDataEvent<ListResponse<CodeRepository>>): Promise<ListResponse<CodeRepository>> {
+            return await this.$api.get<ApiRepositories>(ApiRepositories)
+                .list({lastId: event.lastId, search: event.search}, event.canceler) as ListResponse<CodeRepository>;
         }
     }
 </script>
